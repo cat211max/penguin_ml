5d9128dc6a5a514f19063066e3912b5748430b39
cat211max <64947729+cat211max@users.noreply.github.com>
2025-02-04 16:50:33 +0800

    increase prediction

--- penguins_streamlit_final.py
+++ penguins_streamlit_final.py
@@ -21,3 +21,32 @@ unique_penguin_mapping = pickle.load(map_pickle)
 
 rf_pickle.close()
 map_pickle.close()
+island = st.selectbox("Penguin Island",options = ['Biscoe','Dream','Torgersen'])
+sex = st.selectbox("Sex", options=["Female","Male"])
+bill_length = st.number_input("Bill Length (mm)",min_value=0.0)
+bill_depth = st.number_input("Bill Depth (mm)",min_value=0.0)
+flipper_length = st.number_input("Flipper Length (mm)",min_value=0.0)
+body_mass = st.number_input("Body Mass (g)",min_value=0.0)
+user_inputs = [island,sex,bill_length,bill_depth,flipper_length,body_mass]
+st.write(f"""the user inputs are {user_inputs}""".format())  
+
+# get the inputs into correct format 
+island_biscoe, island_dream, island_torgersen = 0,0,0   
+if island == 'Biscoe':
+    island_biscoe = 1
+elif island == 'Dream':
+    island_dream = 1
+elif island == 'Torgersen':
+    island_torgersen = 1
+sex_female, sex_male = 0,0
+if sex == 'Female':
+    sex_female = 1
+elif sex == 'Male':
+    sex_male = 1
+    
+# prediction 
+new_prediction = rfc.predict([[bill_length,bill_depth,flipper_length,body_mass,island_biscoe,island_dream,island_torgersen,
+                               sex_female,sex_male]])
+
+prediction_species = unique_penguin_mapping[new_prediction][0]
+st.write(f"The predicted species is {prediction_species}")
